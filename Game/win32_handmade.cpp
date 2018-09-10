eee235043716d489c6a0c8c07c97e8e87a18927c
Thiago Gorgulho Leão <burningbluefox@gmail.com>
2018-09-09 23:25:02 -0300

    Formatting

--- Game/win32_handmade.cpp
+++ Game/win32_handmade.cpp
@@ -1,49 +1,17 @@
 #include <Windows.h>
-
-LRESULT CALLBACK MainWindowCallback (
+LRESULT CALLBACK MainWindowCallback
+(
 	HWND   Window,
 	UINT   Message,
 	WPARAM WParam,
 	LPARAM LParam
-)
-{
-	LRESULT Result = 0;
-	switch (Message)
-	{
-		case WM_SIZE:
-			{
-				OutputDebugStringA ("WM_SIZE\n");
-			}
-			break;
+);
 
-		case WM_DESTROY:
-			{
-				OutputDebugStringA ("WM_DESTROY\n");
-			}
-			break;
 
-		case WM_CLOSE:
-			{
-				OutputDebugStringA ("WM_CLOSE\n");
-			}
-			break;
 
-		case WM_ACTIVATEAPP:
-			{
-				OutputDebugStringA ("WM_ACTIVATEAPP\n");
-			}
-			break;
-
-		default:
-			{
-				Result = DefWindowProc (Window, Message, WParam, LParam);
-			}
-			break;
-	}
-	return(Result);
-}
-
-int CALLBACK WinMain (
+// V Entry Point V
+int CALLBACK WinMain
+(
 	HINSTANCE Instance,
 	HINSTANCE PrevInstance,
 	LPSTR     CommandLine,
@@ -54,7 +22,6 @@ int CALLBACK WinMain (
 
 	WNDCLASS WindowClass = {};
 
-	WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
 	WindowClass.lpfnWndProc = MainWindowCallback;
 	WindowClass.hInstance = Instance;
 	//WindowClass.hIcon = ;
@@ -62,11 +29,12 @@ int CALLBACK WinMain (
 
 	if (RegisterClass (&WindowClass))
 	{
-		HWND WindowHandle = CreateWindowEx (
+		HWND WindowHandle = CreateWindowEx
+		(
 			0,
 			WindowClass.lpszClassName,
 			"Handmade Hero",
-			WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+			WS_OVERLAPPEDWINDOW | WS_VISIBLE,
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
@@ -76,13 +44,13 @@ int CALLBACK WinMain (
 			Instance,
 			0
 		);
-		if (WindowHandle!=NULL)
+		if (WindowHandle != NULL)
 		{
 			for (;;)
 			{
 				MSG Message;
 				BOOL MessageResult = GetMessage (&Message, 0, 0, 0);
-				if (MessageResult>0)
+				if (MessageResult > 0)
 				{
 					TranslateMessage (&Message);
 					DispatchMessage (&Message);
@@ -101,4 +69,49 @@ int CALLBACK WinMain (
 	}
 
 	return 0;
+}
+
+
+LRESULT CALLBACK MainWindowCallback
+(
+	HWND   Window,
+	UINT   Message,
+	WPARAM WParam,
+	LPARAM LParam
+)
+{
+	LRESULT Result = 0;
+	switch (Message)
+	{
+	case WM_SIZE:
+	{
+		OutputDebugStringA ("WM_SIZE\n");
+	}
+	break;
+
+	case WM_DESTROY:
+	{
+		OutputDebugStringA ("WM_DESTROY\n");
+	}
+	break;
+
+	case WM_CLOSE:
+	{
+		OutputDebugStringA ("WM_CLOSE\n");
+	}
+	break;
+
+	case WM_ACTIVATEAPP:
+	{
+		OutputDebugStringA ("WM_ACTIVATEAPP\n");
+	}
+	break;
+
+	default:
+	{
+		Result = DefWindowProc (Window, Message, WParam, LParam);
+	}
+	break;
+	}
+	return(Result);
 }
